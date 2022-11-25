9c2ef9d745ce260657ad5dddd71eeb23329d31ca
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-11-25 22:37:13 +0530

    buffer.js

---
+++ nodejs/workspace/lib/buffer.js
@@ -0,0 +1,5 @@
+function testBuffer(){
+    
+}
+
+module.exports = {testBuffer};
--- nodejs/workspace/main.js
+++ nodejs/workspace/main.js
@@ -1,12 +1,14 @@
 const { testFunction, traceFunction } = require("./node-basics/testFunction");
 const { aboutConsoles } = require("./node-basics/console");
 const { logMessage } = require('./lib/logger/logger');
+const {testBuffer} = require('./lib/buffer');
 
 const main = () => {
-    logMessage('starting');
-    aboutConsoles();
-    testFunction();
-    traceFunction();
+    testBuffer();
+    // logMessage('starting');
+    // aboutConsoles();
+    // testFunction();
+    // traceFunction();
 };
 main();
 
