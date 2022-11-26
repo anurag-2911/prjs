a3f4f41fee36faceb5fd37dbcaef0e898cb56883
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-11-26 14:21:05 +0530

    publish npm package.

--- nodejs/workspace/lib/http/http.js
+++ nodejs/workspace/lib/http/http.js
@@ -1,5 +1,18 @@
-function httpTest(){
+function httpTest() {
+  const http = require("http");
 
+  const hostname = "localhost";
+  const port = 6600;
+
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end("Hello World");
+  });
+
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
 }
 
-module.exports = {httpTest};
+module.exports = { httpTest };
