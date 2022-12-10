f25719715eb1e7be275b0ddc07968701dde2e16b
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-12-10 12:18:28 +0530

    initial check-in

---
+++ .NET/DotNETSolution/CommonLibrary/Starter.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonLibrary
+{
+    public class Starter
+    {
+        public static void Start()
+        {
+
+        }
+    }
+}
--- .NET/DotNETSolution/ConsoleApp/Program.cs
+++ .NET/DotNETSolution/ConsoleApp/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CommonLibrary;
+
 Console.WriteLine("Hello, World!");
+Starter.Start();
