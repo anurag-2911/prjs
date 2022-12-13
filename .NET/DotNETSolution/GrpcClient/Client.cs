d9a6d08fd93b5197d222c15ffc407cf926f0b3f3
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-12-13 14:48:27 +0530

    grpc client.

---
+++ .NET/DotNETSolution/GrpcClient/Client.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Core;
+
+
+try
+{
+	Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+	var client = new RemoteService.RemoteServiceClient(channel);
+	RemoteName remoteName=client.GetRemoteName(new RemoteNameRequest{ RemoteId="1"});
+    channel.ShutdownAsync().Wait();
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+
+}
+catch (Exception ex)
+{
+	Console.WriteLine("exception in grpc client "+ ex.ToString());	
+}
