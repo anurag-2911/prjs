4693c385c880b8b3b87b9390acf7161f1e459801
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-12-17 20:16:31 +0530

    spread operator basics.

--- react/test-react/src/App.js
+++ react/test-react/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ const {name,city} = db;
+ const a =[1,2,3];
+ const b= [4,5,6];
+ const c= [...a];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>hello {dosomething()}</p>
+      <p>'name is {name} and city is {city}'</p>
+      <ul>
+     {b.map((x,i)=>{
+      return(<li key={i}>{x}</li>);
+     })}
+      </ul>
+
+      <ul>
+        {c.map((x,i)=>{
+          return (<li key={i}>{x}</li>);
+        })}
+      </ul>
     </div>
   );
 }
+const dosomething = ()=>{return 'hello'};
+const db = {
+  'name':'kunar',
+  'city':'pataal'
 
+}
 export default App;
