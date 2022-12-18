7f37a0b5231a7a5efb70609268936b8fc2c90e2c
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-12-18 13:34:42 +0530

    JS basics.

--- react/test-react/src/App.js
+++ react/test-react/src/App.js
@@ -1,32 +1,51 @@
-import './App.css';
+import "./App.css";
 
 function App() {
- const {name,city} = db;
- const a =[1,2,3];
- const b= [4,5,6];
- const c= [...a];
+  const { name, city } = db;
+  const a = [1, 2, 3];
+  const b = [4, 5, 6];
+  const c = [...a, 99, ...b];
+  const o1 = obj;
+  const clone = { ...o1 };
+  const students = [{
+    'name':'kunar','status':'present'
+  },{'name':'draupadi','status':'present'},{'name':'kans','status':'absent'}]
+
+  const goodStudnets = students.filter(student=>(student.status==='present'))
   return (
     <div className="App">
       <p>hello {dosomething()}</p>
-      <p>'name is {name} and city is {city}'</p>
+      <p>
+        'name is {name} and city is {city} and salay is {clone.salary}'
+      </p>
+      <div>
+        {goodStudnets.map((x,i)=>{
+          return <div key={i}>{x.name} {x.status}</div>
+        })}
+      </div>
       <ul>
-     {b.map((x,i)=>{
-      return(<li key={i}>{x}</li>);
-     })}
+        {b.map((x, i) => {
+          return <li key={i}>{x}</li>;
+        })}
       </ul>
 
       <ul>
-        {c.map((x,i)=>{
-          return (<li key={i}>{x}</li>);
+        {c.map((x, i) => {
+          return <li key={i}>{x}</li>;
         })}
       </ul>
     </div>
   );
 }
-const dosomething = ()=>{return 'hello'};
+const dosomething = () => {
+  return "hello";
+};
 const db = {
-  'name':'kunar',
-  'city':'pataal'
-
-}
+  name: "kunar",
+  city: "pataal",
+};
+const obj = {
+  company: "MF",
+  salary: "100",
+};
 export default App;
