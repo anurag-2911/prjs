c62b26d9edda840921f931f95aca5b61fb7a86fc
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-12-24 14:11:22 +0530

    mongoose data model.

---
+++ nodejs-fullstack/backend/models/user.js
@@ -0,0 +1,26 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  firstName: { type: String, default: null },
+  lastName: { type: String, default: null },
+  userName: { type: String, unique: true },
+  email: { type: String, unique: true },
+  password: { type: String },
+  token: { type: String },
+});
+
+const validate = (user)=>{
+const schema = Joi.object({
+    firstName:Joi.string().required(),
+    lastName:Joi.string().required(),
+    userName:Joi.string().required(),
+    email:Joi.string().required(),
+    password:Joi.string().required(),
+});
+return schema.validate(user);
+}
+
+const User = mongoose.model("user", userSchema);
+
+module.exports ={User,validate};
