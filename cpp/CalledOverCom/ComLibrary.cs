72d30fa6c7d30e12a32d73c321184edbad862adc
Anurag Kumar <kodelabzz.dev@gmail.com>
2023-03-23 21:32:25 +0530

    cc

---
+++ cpp/CalledOverCom/ComLibrary.cs
@@ -0,0 +1,32 @@
+﻿using Novell.Zenworks.AppModule;
+using System;
+using System.Runtime.InteropServices;
+
+namespace CalledOverCom
+{
+    [Guid("225a4f70-2191-4398-8f8d-5172eee5ce1e")]
+    [ComVisible(true)]
+    public interface IComBridge
+    {
+        void DoSomething();
+    }
+
+    [Guid("a31aace2-4213-48ad-8f52-50150c0ff802")]
+    [ComVisible(true)]
+
+    public class ComBridge : IComBridge
+    {
+        AppSettings m_Settings = null;
+        public ComBridge()
+        {
+            m_Settings = new AppSettings();
+        }
+
+        public void DoSomething()
+        {
+
+            Console.WriteLine("C# library method called!");
+        }
+    }
+
+}
---
+++ cpp/TestConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string runtimedir = System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory();
+            Console.WriteLine(runtimedir);
+            string path = System.IO.Path.GetTempPath() + "\\Nallog.txt";
+            File.AppendAllLines(path, new[] { "The very first line!" });
+            Console.ReadKey();
+        }
+    }
+}
