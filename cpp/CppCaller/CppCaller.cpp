3a2e27691cd5cb8a330e0ca7b5440bcc3ceb8887
Anurag Kumar <kodelabzz.dev@gmail.com>
2023-06-18 15:07:28 +0530

    cc

--- cpp/CppCaller/CppCaller.cpp
+++ cpp/CppCaller/CppCaller.cpp
@@ -1,46 +1,71 @@
-#include <windows.h>
-#include <stdio.h>
-#include "Header.h"
-#include <comdef.h>
+#include <iostream>
+#include <Windows.h>
+#include <sddl.h>
+#include <LM.h>
 
+#pragma comment(lib, "Netapi32.lib")
+
+std::wstring GetCurrentUserSidString()
+{
+	HANDLE token = nullptr;
+	DWORD size = 0;
+	TOKEN_USER* tokenUser = nullptr;
+	LPWSTR stringSid = nullptr;
+	std::wstring sidString;
+
+	if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &token))
+	{
+		GetTokenInformation(token, TokenUser, nullptr, 0, &size);
+		tokenUser = (TOKEN_USER*)new BYTE[size];
+		if (GetTokenInformation(token, TokenUser, tokenUser, size, &size))
+		{
+			if (ConvertSidToStringSidW(tokenUser->User.Sid, &stringSid))
+			{
+				sidString = stringSid;
+				LocalFree(stringSid);
+			}
+		}
+		delete[] tokenUser;
+		CloseHandle(token);
+	}
+	return sidString;
+}
+
+std::wstring GetUserUPNBySid(const std::wstring& sidString)
+{
+	std::wstring userUPN;
+	PSID sid;
+	LPWSTR domainName = nullptr;
+	DWORD domainNameSize = 0;
+	SID_NAME_USE sidType;
+	LPWSTR userName = nullptr;
+	DWORD userNameSize = 0;
+
+	if (ConvertStringSidToSidW(sidString.c_str(), &sid))
+	{
+		LookupAccountSidW(nullptr, sid, nullptr, &userNameSize, nullptr, &domainNameSize, &sidType);
+		userName = new WCHAR[userNameSize];
+		domainName = new WCHAR[domainNameSize];
+
+		if (LookupAccountSidW(nullptr, sid, userName, &userNameSize, domainName, &domainNameSize, &sidType))
+		{
+			userUPN = userName;
+			userUPN += L'@';
+			userUPN += domainName;
+		}
+
+		delete[] userName;
+		delete[] domainName;
+		LocalFree(sid);
+	}
+	return userUPN;
+}
 
 int main()
 {
-	CoInitialize(NULL);
-
-	CLSID clsid;
-	IID iid;
-	HRESULT hr;
-
-	hr = CLSIDFromProgID(L"CalledOverCom.ComBridge", &clsid);
-	if (FAILED(hr))
-	{
-		printf("CLSIDFromProgID failed with error 0x%x\n", hr);
-		return 1;
-	}
-
-	hr = IIDFromString(L"{225a4f70-2191-4398-8f8d-5172eee5ce1e}", &iid);
-	if (FAILED(hr))
-	{
-		printf("IIDFromString failed with error 0x%x\n", hr);
-		return 1;
-	}
-
-	CalledOverCom::IComBridge* comBridge = NULL;
-	hr = CoCreateInstance(clsid, NULL, CLSCTX_INPROC_SERVER, iid, (LPVOID*)&comBridge);
-	if (FAILED(hr))
-	{
-		_com_error err(hr);
-		LPCTSTR errMsg = err.ErrorMessage();
-		printf("CoCreateInstance failed with error 0x%x\n", hr);
-		return 1;
-	}
-
-	comBridge->DoSomething();
-
-	comBridge->Release();
-
-	CoUninitialize();
-
+	std::wstring sidString = GetCurrentUserSidString();
+	std::wstring userUPN = GetUserUPNBySid(sidString);
+	std::wcout << L"User Principal Name: " << userUPN << std::endl;
+	system("pause");
 	return 0;
 }
